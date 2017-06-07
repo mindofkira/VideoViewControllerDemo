5ac6bf408d0b621a5ee135ec89cad5ec7f7e6579
Light Yagami <mindofkira@gmail.com>
2017-06-07 21:37:38 +0530

    buffering

--- app/src/main/java/com/bgautam/videostreamer/MainActivity.java
+++ app/src/main/java/com/bgautam/videostreamer/MainActivity.java
@@ -3,6 +3,7 @@ package com.bgautam.videostreamer;
 import android.content.res.AssetFileDescriptor;
 import android.graphics.Matrix;
 import android.graphics.SurfaceTexture;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.CountDownTimer;
@@ -21,7 +22,7 @@ import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity implements TextureView.SurfaceTextureListener {
 
-    private static final String FILE_URL = "http://www.androidbegin.com/tutorial/AndroidCommercial.3gp";
+    private static final String FILE_URL = "https://r2---sn-5jucgv5qc5oq-jwce.googlevideo.com/videoplayback?ratebypass=yes&expire=1496872298&lmt=1485241602738874&mime=video%2Fmp4&key=cms1&pl=24&ipbits=0&requiressl=yes&ei=CiE4WcvODNOG1gKk_4GABA&itag=18&dur=307.014&id=o-ABJFmsfYzmsYFMq7GkrUmkcJYC90y3JcAFxLRy7OTKb6&gir=yes&sparams=clen,dur,ei,expire,gir,id,ip,ipbits,ipbypass,itag,lmt,mime,mip,mm,mn,ms,mv,pcm2cms,pl,ratebypass,requiressl,source&source=youtube&clen=18697299&ip=159.253.144.86&signature=3C6A0C2A1D9530914E36D698AF9899F0E63565CF.08CF07702B9F969B4AA03D17E6BCD96CE8DAD352&title=Guzarish+%28Full+Song%29+Ghajini+feat.+Aamir+Khan&redirect_counter=1&req_id=ae86d92edbd3a3ee&cms_redirect=yes&ipbypass=yes&mip=49.205.140.24&mm=31&mn=sn-5jucgv5qc5oq-jwce&ms=au&mt=1496850643&mv=m&pcm2cms=yes";
     private static final String TAG = MainActivity.class.getName();
     private MediaPlayer mMediaPlayer;
     private LinearLayout mLinear_layout;
@@ -53,7 +54,6 @@ public class MainActivity extends AppCompatActivity implements TextureView.Surfa
             mToggleButton.setEnabled(true);
             mToggleButton.setClickable(true);
             adjustAspectRatio(mMediaPlayer.getVideoWidth(), mMediaPlayer.getVideoHeight());
-
         }
     }
 
@@ -74,6 +74,8 @@ public class MainActivity extends AppCompatActivity implements TextureView.Surfa
         try {
             mMediaPlayer = new MediaPlayer();
             mMediaPlayer.setDataSource(getApplicationContext(), Uri.parse(FILE_URL));
+            mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            mMediaPlayer.setScreenOnWhilePlaying(true);
             mMediaPlayer.setSurface(surface);
             mMediaPlayer.setLooping(false);
             mToggleButton.setEnabled(true);
@@ -114,6 +116,8 @@ public class MainActivity extends AppCompatActivity implements TextureView.Surfa
                     mToggleButton.setClickable(false);
                 }
             });
+
+
 
         } catch (IllegalArgumentException e) {
             Log.d(TAG, e.getMessage());
